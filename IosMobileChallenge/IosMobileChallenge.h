a47a4b75bda386389b203dc812b9935a3d0e4b0e
Elechim01 <68447158+Elechim01@users.noreply.github.com>
2021-12-11 10:40:52 +0100

    Initial Commit

---
+++ IosMobileChallenge/IosMobileChallenge.h
@@ -0,0 +1,18 @@
+//
+//  IosMobileChallenge.h
+//  IosMobileChallenge
+//
+//  Created by Michele Manniello on 11/12/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for IosMobileChallenge.
+FOUNDATION_EXPORT double IosMobileChallengeVersionNumber;
+
+//! Project version string for IosMobileChallenge.
+FOUNDATION_EXPORT const unsigned char IosMobileChallengeVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <IosMobileChallenge/PublicHeader.h>
+
+
